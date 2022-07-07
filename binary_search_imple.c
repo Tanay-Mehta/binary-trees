7c3ce82b30db9818b1dc48be267eaa5282c09a02
Tanay Mehta <mehtatanay30@gmail.com>
2022-07-07 20:20:10 +0530

    binary search trees simple implementation

--- binary_search_imple.c
+++ binary_search_imple.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node{
+typedef struct node
+{
     int data;
-    struct node* left;
-    struct node* right;
-}node;
+    struct node *left;
+    struct node *right;
+}
+Node;
 
-node* Newnode(int data){
-    node *newnode = malloc(sizeof(node));
+Node *Newnode(int data){
+    Node *newnode = malloc(sizeof(Node));
     newnode->left = NULL;
     newnode->right = NULL;
     newnode->data = data;
     return newnode;
 }
-
-void main(){
-    node *root = Newnode(5);
+struct node *root = NULL;
+void search(int value){
+    struct node *root_ref = root;
+    while(root_ref!=NULL){
+        if(root_ref->data==value){
+            printf("found");
+            break;
+        }
+        else if(root_ref->data>value){
+            
+            root_ref = root_ref->left;
+        }
+        else if(root_ref->data<value){
+            
+            root_ref = root_ref->right;
+        }
+        }
+    
+}
+void main(void){
+    root = Newnode(5);
     root->left = Newnode(3);
-    root->right = Newnode(8);
+    root->right = Newnode(7);
     root->left->left = Newnode(1);
-    root->left->left = Newnode(4);
+    root->left->right = Newnode(4);
     root->right->left = Newnode(6);
-    root->right->right = Newnode(9);
+    root->right->right = Newnode(8);
+    search(1);
 }
